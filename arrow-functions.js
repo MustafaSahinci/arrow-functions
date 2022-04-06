0c515bc56499f7ab73eb739da1fa97cf078c0d60
Mustafa <mustafa_sahinci@hotmail.com>
2022-04-06 23:54:00 +0000

    add arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumber(a, b) {
+    return a + b
+}
 
+let sum = addTwoNumber(4, 4)
+console.log(sum)
 // Arrow Function With Parameters
+const adddTwoNumber = (a, b) => {
+    return a + b
+}
 
+let summ = addTwoNumber(4, 4)
+console.log(summ)
 // Single Line Arrow Function With Parameters
+const adddTwoNumber2 = (a, b) => a+b
 
+let sum2 = adddTwoNumber2(3,3)
+console.log(sum2)
 // Implicit Returns
+const saySomething = message => console.log(message)
 
+saySomething("hello je moeder")
+
+const hello = () => console.log("hello")
+hello()
 // Returning Multiple Lines
+const moreLines = () => (
+    `<p>
+        this is a multiline string1
+    </p>`
+)
+console.log(moreLines())
